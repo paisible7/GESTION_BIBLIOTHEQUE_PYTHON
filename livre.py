9e4d3722acdb294635e4e1456271aa39ae99027a
paisible7 <22km127@esisalama.org>
2024-07-01 20:31:12 +0200

    COMMIT_01_JUILLET = initialisation des fonctions pour ajouter un livre

---
+++ livre.py
@@ -0,0 +1,23 @@
+def chargerLivres():
+    pass
+
+def sauvegarderLivres():
+    pass
+
+def afficherLivres():
+    pass
+
+def validerTitre():
+    pass
+
+def validerAuteur():
+    pass
+
+def validerGenre(genre):
+    pass
+
+def livreExiste():
+    pass
+
+def ajouterLivre():
+    pass
