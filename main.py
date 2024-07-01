248775e56869f659ec8d7434b64f87117b29c88b
paisible7 <22km127@esisalama.org>
2024-07-01 20:09:41 +0200

    COMMIT_01_JUILLET = ajout du menu dans la fonction main

---
+++ main.py
@@ -0,0 +1,36 @@
+def menu():
+    print("1. Ajouter un livre\n")
+    print("2. Chercher un livre\n")
+    print("3. supprimer un livre\n")
+    print("4. Afficher un livre\n")
+    print("5. Emprunter un livre\n")
+    print("6. Retourner un livre\n")
+    print("7. Quitter le programme\n")
+    choix = int(input("\tSelectionner une option : "))
+    return choix
+
+def main():
+    while True :
+        choix = menu()
+        if choix == 1:
+            pass
+            pass
+        elif choix == 2:
+            pass
+        elif choix == 3:
+            pass
+        elif choix == 4:
+            pass
+        elif choix == 5:
+            pass
+        elif choix == 6:
+            pass
+        elif choix == 7:
+            pass
+        else :
+            print("Veuillez choisir une option valide\n")
+
+if __name__ == "__main__":
+    main()
+
+    
